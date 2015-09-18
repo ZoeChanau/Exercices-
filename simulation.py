694f9083a522b6c21a7d4d965153c3db9b708c21
ZoeChanau <zoe.chanau@wanadoo.fr>
2015-09-18 09:29:48 +0200

    Create simulation.py
    Simulation de données avec une boucle

---
+++ simulation.py
@@ -0,0 +1,11 @@
+from random import random
+import numpy as np
+
+v= [] #liste vide
+N=25 #nombre fin boucle
+
+for i in range (N) :
+	v.append(10*random()) #ajout d'un element 
+
+print type(v)
+print v
